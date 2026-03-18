6bba7e0e500fcdf928b8d1713d43fa54bdd27f34
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-03-19 03:02:36 +0530

    Made a setup for the project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>This is a macos portfolio</h1>
+    </>
+  )
+}
+
+export default App
