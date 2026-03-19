2102ee33bf5fc0b7756e9b0019ea2cfc37e17e9e
tusharsingla22222-source <tusharsingla22222@gmail.com>
2026-03-19 16:05:35 +0530

    feat: Implement Navbar component, centralize application constants, and configure path aliases for improved imports.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
+import Navbar from '#components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>This is a macos portfolio</h1>
-    </>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
